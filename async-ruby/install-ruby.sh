b3da620b70d3c7d0ea770591047da3c6b774038b
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-04-01 15:28:01 -0400

    Update install-ruby.sh

--- async-ruby/install-ruby.sh
+++ async-ruby/install-ruby.sh
@@ -1,14 +1,11 @@
 echo "[*] Before we can get started, we need to install Ruby!"
-sleep 3
-
 echo "[*] Downloading required packages (curl / gnupg2):"
 apt install curl gnupg2 -y
-
-echo "[*] downloading the ruby version manager (rvm) and ruby:"
+echo "[*] Adding required GPG key:"
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+echo "[*] Downloading the Ruby Version Manager (rvm) and Ruby:"
 curl -sSL https://get.rvm.io | bash -s stable --ruby
 source /usr/local/rvm/scripts/rvm
-
 clear
-
 echo "Done installing Ruby!"
 ruby -v
