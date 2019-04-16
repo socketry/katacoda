daaad00ba1e2d75041a113215e4fcdea28016359
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-04-16 15:44:24 +0900

    Use rvm head to get Ruby 2.6.2

--- async-introduction/install-ruby.sh
+++ async-introduction/install-ruby.sh
@@ -1,1 +1,1 @@
-apt install gnupg2 -y && gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB && (curl -sSL https://get.rvm.io | bash -s stable --ruby) && source /usr/local/rvm/scripts/rvm && clear && ruby -v
+apt install gnupg2 -y && gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB && (curl -sSL https://get.rvm.io | bash -s head --ruby) && source /usr/local/rvm/scripts/rvm && clear && ruby -v
